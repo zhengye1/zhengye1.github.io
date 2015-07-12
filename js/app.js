706148760fea5750621cdb53ac960946027a539d
zhengye1 <ye.zheng@mail.utoronto.ca>
2015-07-12 02:06:17 -0400

    Add a contact page and try angular

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('VincentGitPage', ['ngRoute'])
+				.config(['$routeProvider', 
+					function($routeProvider){
+						$routeProvider.
+							when('/contact'. {
+								templateUrl: 'views/contact.html'
+							})
+					}]);
